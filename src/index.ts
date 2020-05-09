ce2f8cc42894a3dea2629f27eedff08b190db864
tzhao <thomas.zhao@amadeus.com>
2020-05-09 14:04:36 +0200

    Add print file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import {writeFileSync} from 'fs';
 import * as http from "http";
 import {RequestOptions} from "https";
 import * as https from 'https';
+import {createFileSync} from 'fs-extra';
 
 export interface CGFrame {
   agentId: number;
@@ -26,9 +28,11 @@ export interface CGResponse {
   frames: CGFrame[];
 }
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 console.log(process.argv);
-const gameIdentifier: string = `[${process.argv[2]},null]`;
+const gameId = process.argv[2];
+const gameIdentifier: string = `[${gameId},null]`;
 
 const options: RequestOptions = {
   hostname: 'www.codingame.com',
@@ -39,17 +43,33 @@ const options: RequestOptions = {
     "Host": " www.codingame.com",
     "Connection": " keep-alive",
     "Accept": " application/json, text/plain, */*",
-    "Content-Type": [" application/json;charset=UTF-8", "application/json"],
+    "Content-Type": [" application/json;charset=UTF-8"],
     "Accept-Encoding": " gzip, deflate, br",
     "Accept-Language": " en-US,en;q=0.9"
   }
 };
 
-const req: http.ClientRequest = https.request(options, (res) => {
+const req: http.ClientRequest = https.request(options, (res: http.IncomingMessage) => {
   console.log(`statusCode: ${res.statusCode}`);
-
+  res.setEncoding('UTF-8');
   res.on('data', (d: CGResponse) => {
     console.log(d);
+    if (d.agents && d.frames) {
+      d.agents.forEach((agent: CGAgent) => {
+        const userId:string = agent.codingamer.userId.toString();
+        const userIndex: number = agent.index;
+        let output: string = '';
+        for (const frame of d.frames) {
+          if (frame.agentId === userIndex && !!frame.stderr) {
+            output += frame.stderr;
+            output += '\n';
+          }
+        }
+        const file: string = `./target/${gameId}-${userId}`;
+        createFileSync(file);
+        writeFileSync(file, output);
+      });
+    }
   })
 });
 
@@ -57,5 +77,5 @@ req.on('error', (error) => {
   console.error(error);
 });
 
-req.write(JSON.stringify(gameIdentifier));
+req.write(gameIdentifier);
 req.end();
